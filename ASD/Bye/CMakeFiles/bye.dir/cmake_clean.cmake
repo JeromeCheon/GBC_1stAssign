8ca1dcf6a397ff59aea2b64863795b45f0c05a2f
JeromeCheon <21400749@handong.edu>
2017-12-18 17:17:40 +0900

    initialize

---
+++ ASD/Bye/CMakeFiles/bye.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bye.dir/Bye.cpp.o"
+  "bye.pdb"
+  "bye"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bye.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
